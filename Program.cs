55ba575ea8ec21e122a88168cc84c9629a52a2b8
Cristian Malaga <cmalaga@usf.edu>
2019-09-05 16:50:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InClassproject
+{
+    class leave
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Console.WriteLine("How many days do you want to take of work");
+                int days = int.Parse(Console.ReadLine());
+                if (days > 7)
+                {
+                    Console.WriteLine("Sorry your leave is denied !");
+                }
+                else
+                {
+                   if (days >= 0 & days <= 7)
+                    {
+                        Console.WriteLine("you can leave");
+                    }
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("There is an ERROR IN THE CODE!");
+            }
+        }
+    }
+}
